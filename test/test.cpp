7e25d9cfcd3727ee87b2bd22c2a55f3dea4f4106
lukasst <lukas.steiger@hotmail.com>
2015-12-02 09:34:13 +0100

    setup testat 3

---
+++ test/test.cpp
@@ -0,0 +1,27 @@
+#include "cute.h"
+#include "ide_listener.h"
+#include "xml_listener.h"
+#include "cute_runner.h"
+#include "indexableSet.h"
+
+void test() {
+    ASSERT_EQUAL(1, 1);
+}
+
+void runAllTests(int argc, char const *argv[]){
+    cute::suite s;
+    // Word.h
+    s.push_back(CUTE(test));
+
+    cute::xml_file_opener xmlfile(argc,argv);
+    cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
+    cute::makeRunner(lis,argc,argv)(s, "AllTests");
+}
+
+int main(int argc, char const *argv[]){
+    runAllTests(argc,argv);
+    return 0;
+}
+
+
+
