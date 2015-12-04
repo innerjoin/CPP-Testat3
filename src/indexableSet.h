c40ae523bbdbd950b1136770bdd0ee44acd4c245
lukasst <lukas.steiger@hotmail.com>
2015-12-04 11:12:52 +0100

    more test cases added & bugs fixed

--- src/indexableSet.h
+++ src/indexableSet.h
@@ -10,6 +10,7 @@ class IndexableSet : public std::set<T, COMPARE> {
     using size_type = typename IdxSet::size_type;
 
 public:
+    using IdxSet::set;
 
     T const & front() const {
         return at(0);
@@ -19,8 +20,10 @@ public:
         return at(this->size() - 1);
     }
 
-    T const & at(size_type index) const {
-        if (index >= this->size() || -index > this->size())
+    T const & at(int index) const {
+        size_t size = this->size();
+        //if(index >= size || -index > size)
+        if ((index >= 0 && index >= size) || (index < 0 && -index > size))
             throw std::out_of_range{"index out of range!!"};
         size_t idx = index >= 0 ? index : this->size() + index;
         auto iterator = this->begin();
--- test/test.cpp
+++ test/test.cpp
@@ -4,15 +4,51 @@
 #include "cute_runner.h"
 #include "indexableSet.h"
 
-void test() {
-    //IndexableSet ix;
-    ASSERT_EQUAL(1, 1);
+void testIndexableSetAccess() {
+    IndexableSet<char> is{'b', 'e', 'e', 'r'};
+    ASSERT_EQUAL(3, is.size());
+    ASSERT_EQUAL('b', is[0]);
+    ASSERT_EQUAL('e', is[1]);
+    ASSERT_EQUAL('r', is[2]);
+}
+
+void testEmptyIndexableSet() {
+    IndexableSet<int> is{};
+    ASSERT_EQUAL(0, is.size());
+    ASSERT(is.empty());
+}
+
+void testIndexableSetFrontBack() {
+    IndexableSet<int> is{88, -10, 23, 6};
+    ASSERT_EQUAL(4, is.size());
+    ASSERT_EQUAL(-10, is.front());
+    ASSERT_EQUAL(88, is.back());
+}
+
+void testIndexableSetNegativeIndexAccessFromBack() {
+    IndexableSet<char> is{'b', 'e', 'e', 'r'};
+    ASSERT_EQUAL('r', is.at(-1));
+    ASSERT_EQUAL('e', is.at(-2));
+    ASSERT_EQUAL('b', is.at(-3));
+}
+
+void testIndexableSetOverflowCapture() {
+    IndexableSet<int> is{1, 2, 3, 4};
+    ASSERT_THROWS(is.at(4), std::out_of_range);
+    ASSERT_THROWS(is.at(-5), std::out_of_range);
+
+    IndexableSet<char> is2{};
+    ASSERT_THROWS(is2.at(0), std::out_of_range);
+    ASSERT_THROWS(is2.at(-1), std::out_of_range);
 }
 
 void runAllTests(int argc, char const *argv[]){
     cute::suite s;
-    // Word.h
-    s.push_back(CUTE(test));
+    s.push_back(CUTE(testIndexableSetAccess));
+    s.push_back(CUTE(testEmptyIndexableSet));
+    s.push_back(CUTE(testIndexableSetFrontBack));
+    s.push_back(CUTE(testIndexableSetNegativeIndexAccessFromBack));
+    s.push_back(CUTE(testIndexableSetOverflowCapture));
 
     cute::xml_file_opener xmlfile(argc,argv);
     cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
@@ -23,6 +59,3 @@ int main(int argc, char const *argv[]){
     runAllTests(argc,argv);
     return 0;
 }
-
-
-
